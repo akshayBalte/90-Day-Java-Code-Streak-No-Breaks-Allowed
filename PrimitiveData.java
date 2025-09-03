257715774ee019fd38babfdf451a5610e7716059
akshay balte <akshay.balte.cmaug25@gmail.com>
2025-09-03 19:21:50 +0530

    Add files via upload

---
+++ PrimitiveData.java
@@ -0,0 +1,37 @@
+class PrimitiveData{
+	
+	
+		public static void main(String args[]){
+			
+			byte b =127;
+			short s = 345;
+			int  i =66666;
+			long l =99999999999L;
+			float f = 45.005f;
+			double d = 345.456789;
+			boolean b1 = false;
+			char ch = 'A';
+			
+			System.out.println("Byte = "+b);
+			System.out.println("Short = "+s);
+			System.out.println("Int= "+i);
+			System.out.println("Long = "+l);
+			System.out.println("Float = "+f);
+			System.out.println("Double = "+d);
+			System.out.println("Boolean = "+b1);
+			System.out.println("Char = "+ch);
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
